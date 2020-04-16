a5f147d710149f2079ac8738c97a191988f6de6f
josephlee3454 <joseph.lee3454@gmail.com>
2020-04-16 10:55:11 -0700

    lab-7-done

--- server.js
+++ server.js
@@ -15,57 +15,62 @@ app.use(cors());// invokes cors
 const PORT = process.env.PORT || 3001; // defines port as either whats inside .env or 3001
 
 
-
+//location
 
 //application gets location and sends a request to server
 app.get('/location', (request, response) => {
 
-  try{// tests for errors 
     let city = request.query.city;//grab city data from qury
     let key = process.env.GEOCODE_API_KEY;
-    console.log('city', city)
+    console.log('city', city);
     let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
     superagent.get(url)
         .then(results => {
-          let searchCity = results.body[0];
-          console.log('searchCity, city');
-          let location = new Location(searchCity, city);
+          let location = new Location(results.body[0]);
           console.log('locationData', location);
           response.status(200).send(location);
-        })
-  }
-  catch(err){// if it fails the error test it sets it below
-    response.status(500).send(err)// sends error 500 
-    console.error('you messed up')
-  }
+        }).catch(err => {// if it fails the error test it sets it below
+    response.status(500).send(err);// sends error 500 
+    console.error('you messed up');
+  })
 })
-
+///weather
 app.get('/weather', (request, response) =>{
+  console.log(request.query)
   // get data from the darksky file
-  let weather = [];
   let lat = request.query.latitude;
-  let lon = request.query.longitude
-  let key = process.env.WEATHER_API_KEY
-  let Weatherurl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
+  let lon = request.query.longitude;
+  let key = process.env.WEATHER_API_KEY;
+  let Weatherurl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}&days=7`;
+  
   superagent.get(Weatherurl)
   .then(results => {
-    let wData = results.body.daily.data;
-    wData.map(day =>{
-      let newDay = new Weather(day);
-      weather.push(newDay);
-    })
-    response.status(200).send(weather);
-  });
+    console.log(results);
+    let wData = results.body.data;
+    let wArr = wData.map(day =>{
+      return new Weather(day);
+     })
+    response.status(200).send(wArr);
+  }).catch(err=>{
+    console.log('its messed up', err)
+    response.send(err)
+  })
+})
+app.get('/trails', (request, response) => {
+  let {latitude,longitude} = request.query;
+  let trailKey =process.env.TRAILS_API_KEY;
+  let urlTrail = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${trailKey}`;
+ 
+  superagent.get(urlTrail)
+    .then(results => {
+      let trail = results.body.trails;
+      const dataObj = trail.map(trail => new Trail(trail));
+      response.status(200).send(dataObj);
+    }).catch(err=>{
+      console.log('its messed up', err)
+      response.send(err)
+    });
 });
-// app.get('/trails', (request, response) => {
-//   let {latitude,longitude} = request.query;
-//   let urlTrail = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${process.env.TRAILS_API_KEY}`;
-//   superagent.get(urlTrail)
-//     .then(results => {
-//       const dataObj = results.body.trails.map(trail => new trail(trail));
-//       response.status(200).send(dataObj);
-//     });
-// });
 
 //constructor function
 function Location(obj, city){
@@ -76,20 +81,20 @@ function Location(obj, city){
 }
 
 function Weather(obj){
-  this.time = new Date(obj.time * 1000).toDateString();
-  this.forecast = obj.summary;
+  this.time = new Date(obj.datetime * 1000).toDateString();
+  this.forecast = obj.weather.description;
 }
-// function Trail(obj){
-//   this.name = obj.name;
-//   this.location = obj.location;
-//   this.length = obj.length;
-//   this.stars = obj.stars;
-//   this.summary = obj.summary;
-//   this.trail_url = obj.url;
-//   this.conditions = obj.conditionStatus;
-//   this.condition_date = obj.conditionDate.slice(0,10);
-//   this.condition_time = obj.conditionDate.slice(11,19);
-// }
+function Trail(obj){
+  this.name = obj.name;
+  this.location = obj.location;
+  this.length = obj.length;
+  this.stars = obj.stars;
+  this.summary = obj.summary;
+  this.trail_url = obj.url;
+  this.conditions = obj.conditionStatus;
+  this.condition_date = obj.conditionDate.slice(0,10);
+  this.condition_time = obj.conditionDate.slice(11,19);
+}
 
 //if response is a 404 messeage then the log will say 
 app.get('*',(request,response)=>{
