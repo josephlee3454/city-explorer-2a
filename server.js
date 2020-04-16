6f8c03ace5d81debe0028261cabb3b5791960662
josephlee3454 <joseph.lee3454@gmail.com>
2020-04-15 22:21:15 -0700

    added apis

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.get('/location', (request, response) => {
   try{// tests for errors 
     let city = request.query.city;//grab city data from qury
     let key = process.env.GEOCODE_API_KEY;
-    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
     console.log('city', city)
+    let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
     superagent.get(url)
         .then(results => {
           let searchCity = results.body[0];
@@ -41,31 +41,31 @@ app.get('/location', (request, response) => {
 })
 
 app.get('/weather', (request, response) =>{
-  // figure out what the front end sent us
-  // console.log('this is the information the front end sent us', request.query);
-  // { search_query: 'seattle',
-  // formatted_query: 'Lynnwood, Snohomish County, Washington, USA',
-  // latitude: '47.8278656',
-  // longitude: '-122.3053932' }
-  // let city = request.query.search_query;
-  // let formatted_query = request.query.formatted_query;
-  // let latitude = request.query.latitude;
-  // let longitude = request.query.longitude;
-
-
   // get data from the darksky file
-  let weather = require('./data/darksky.json');
-  let weatherArray = weather.daily.data;
-
-  // run it through a constructor function
-  const finalWeatherArray = weatherArray.map(day => {
-    return new Weather(day);
-  })
-  // send it to the frontend
-  response.send(finalWeatherArray);
-})
-
-
+  let weather = [];
+  let lat = request.query.latitude;
+  let lon = request.query.longitude
+  let key = process.env.WEATHER_API_KEY
+  let Weatherurl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
+  superagent.get(Weatherurl)
+  .then(results => {
+    let wData = results.body.daily.data;
+    wData.map(day =>{
+      let newDay = new Weather(day);
+      weather.push(newDay);
+    })
+    response.status(200).send(weather);
+  });
+});
+// app.get('/trails', (request, response) => {
+//   let {latitude,longitude} = request.query;
+//   let urlTrail = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${process.env.TRAILS_API_KEY}`;
+//   superagent.get(urlTrail)
+//     .then(results => {
+//       const dataObj = results.body.trails.map(trail => new trail(trail));
+//       response.status(200).send(dataObj);
+//     });
+// });
 
 //constructor function
 function Location(obj, city){
@@ -79,6 +79,17 @@ function Weather(obj){
   this.time = new Date(obj.time * 1000).toDateString();
   this.forecast = obj.summary;
 }
+// function Trail(obj){
+//   this.name = obj.name;
+//   this.location = obj.location;
+//   this.length = obj.length;
+//   this.stars = obj.stars;
+//   this.summary = obj.summary;
+//   this.trail_url = obj.url;
+//   this.conditions = obj.conditionStatus;
+//   this.condition_date = obj.conditionDate.slice(0,10);
+//   this.condition_time = obj.conditionDate.slice(11,19);
+// }
 
 //if response is a 404 messeage then the log will say 
 app.get('*',(request,response)=>{
